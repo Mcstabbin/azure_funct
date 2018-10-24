b3a9b75e8932e490489c415a594244f74725891a
Jacob McPhillips <jacob.mcphillips@gmail.com>
2018-10-23 21:06:19 -0700

    changing the dingus

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hi " + (req.query.name || req.body.name)
+            body: "Dont be a dingus " + (req.query.name || req.body.name)
         };
     }
     else {
